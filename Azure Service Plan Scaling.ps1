4e58084432dd5c20b15c27aee0a3005722699e07
4lenz1 <pingfallenzone@gmail.com>
2017-01-17 03:14:24 +0900

    Initial
    upload

---
+++ Azure Service Plan Scaling.ps1
@@ -0,0 +1,36 @@
+﻿$AzureSubscriptionName = "Subsription"
+$AzureServiceAppName = "Service Name"
+$ResourceGroupName = 	"Resource Group"
+$tier = "Shared" # Free / Shared / Basic / Standard
+
+
+$connectionName = "automation"
+try
+{
+    # Get the connection "AzureRunAsConnection "
+    $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
+
+    "Logging in to Azure..."
+    Add-AzureRmAccount `
+        -ServicePrincipal `
+        -TenantId $servicePrincipalConnection.TenantId `
+        -ApplicationId $servicePrincipalConnection.ApplicationId `
+        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+}
+catch {
+    if (!$servicePrincipalConnection)
+    {
+        $ErrorMessage = "Connection $connectionName not found."
+        throw $ErrorMessage
+    } else{
+        Write-Error -Message $_.Exception
+        throw $_.Exception
+    }
+}
+
+ "changing service plan"
+
+Set-AzureRmAppServicePlan -Name  $AzureServiceAppName -ResourceGroupName $ResourceGroupName -Tier $tier
+
+"complete"
+
